042c51253c47adb0afa8978a71c5f805f5f5e5c9
ich777 <ich777@noreply.example.org>
2019-08-04 11:40:57 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-if [ ! -f ${STEAMCMD_DIR}/steamcmd.sh ]; then
-    echo "SteamCMD not found!"
-    wget -q -O ${STEAMCMD_DIR}/steamcmd_linux.tar.gz http://media.steampowered.com/client/steamcmd_linux.tar.gz 
-    tar --directory ${STEAMCMD_DIR} -xvzf /serverdata/steamcmd/steamcmd_linux.tar.gz
-    rm ${STEAMCMD_DIR}/steamcmd_linux.tar.gz
-fi
-
 
 echo "---Prepare Server---"
 chmod -R 770 ${DATA_DIR}
